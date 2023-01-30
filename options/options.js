9d91759c487199fc3fa9537d43ff04885b7ccc5c
Sai Vuppalapati <saiaditya.tr@gmail.com>
2023-01-30 15:18:52 -0600

    Crowdbotics Studio: Update app screens

--- options/options.js
+++ options/options.js
@@ -1,12 +1,10 @@
-import { StyleSheet } from "react-native"
-
 export const globalOptions = {
   name: "magic-meals-38580",
   url: "https://magic-meals-38580.botics.co",
   api: "https://magic-meals-38580.botics.co/api/v1"
-}
+};
 export const modulesOptions = {
   "@modules/app-menu": {
     copy: "Routes available!"
   }
-}
+};
